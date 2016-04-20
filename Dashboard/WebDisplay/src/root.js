cce2056fcbf66dc99a8a2f1e72fe1349e7f3a541
Charlotte Weaver <weaverc10@janelia.hhmi.org>
2016-04-20 17:18:55 -0400

    Added redux store

--- Dashboard/WebDisplay/src/root.js
+++ Dashboard/WebDisplay/src/root.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import webDisplayApp from './reducers'
 import App from './app'
+console.log(webDisplayApp)
 
+let store = createStore(webDisplayApp);
 
 render(
-	<App />,
-  document.getElementById('content')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('content')
 );
